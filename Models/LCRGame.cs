4534be2ed58b4bba2ad9f497d9a1f82052c2db48
Efrain Marin <efrain.marin@shmerida.mx>
2021-06-15 21:26:59 -0500

    Unit Test project added

---
+++ Models/LCRGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfChallenge.Models
+{
+    public class LCRGame
+    {
+        public LCRGame(int numberOfPlayers = 3)
+        {
+            if (numberOfPlayers < 3)
+                throw new Exception("The minimum number of required players is 3");
+
+            this._numberOfPlayers = numberOfPlayers;
+        }
+
+        private int _numberOfPlayers;
+
+        public int NumberOfPlayers
+        {
+            get { return _numberOfPlayers; }
+            private set { _numberOfPlayers = value; }
+        }
+
+
+
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfChallenge.Models
+{
+    public class Player
+    {
+        public Player()
+        {
+        }
+    }
+}
